1b43701e4e7225130b1d7cc200a114732d47594c
Yang <yanglin501st@yahoo.com>
2016-01-19 18:37:40 -0500

    Added a comment

--- Team3044-2016/src/com/team3044/robotmain/Robot.java
+++ Team3044-2016/src/com/team3044/robotmain/Robot.java
@@ -23,6 +23,7 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+    //Yang says hi
     public void robotInit() {
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", defaultAuto);
