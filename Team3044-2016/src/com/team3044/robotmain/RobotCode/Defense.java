0c6bd41b7f2259db189cfb88b45b1f91a64fb906
Yang <yanglin501st@yahoo.com>
2016-01-19 19:15:21 -0500

    Added Files

---
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Defense.java
@@ -0,0 +1,49 @@
+
+package com.team3044.robotmain.RobotCode;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot {
+    final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser chooser;
+	
+    public void robotInit() {
+        chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+        SmartDashboard.putData("Auto choices", chooser);
+    }
+    
+    public void autonomousInit() {
+    	autoSelected = (String) chooser.getSelected();
+    	//autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);
+    }
+
+   
+    public void autonomousPeriodic() {
+    	switch(autoSelected) {
+    	case customAuto:
+        //Put custom auto code here   
+            break;
+    	case defaultAuto:
+    	default:
+    	//Put default auto code here
+            break;
+    	}
+    }
+
+    public void teleopPeriodic() {
+        
+    }
+    
+
+    public void testPeriodic() {
+    
+    }
+}
