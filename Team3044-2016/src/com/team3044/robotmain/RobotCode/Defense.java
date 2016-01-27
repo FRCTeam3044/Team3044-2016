d6d738fd7868c2412839e062288e1764c5614c95
Ryan Keihm <TheRyGuy125@gmail.com>
2016-01-27 18:55:54 -0500

    added final states for the case structure of the defense system

--- Team3044-2016/src/com/team3044/robotmain/RobotCode/Defense.java
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Defense.java
@@ -6,31 +6,65 @@ import com.team3044.robotmain.Reference.*;
 public class Defense {
 	Components components = new Components();
 	SecondaryController secondJoy = SecondaryController.getInstance();
-    public void defenseInit() {
-    
-    	
-	final int MOVING = 1;
-	final int STOPPING = 2;
-	final int HOME = 3;
-	final int SALLY_PORT1 = 4;
-	final int SALLY_PORT2 = 5;
-	final int DRAW_BRIDGE1 = 6;
-	final int DRAWBRIDGE2 = 7;
-	final int OTHER = 8;
-	
-	int DEFENSE_STATE = 9;
-    }
-    
-    public void defenseAutoPeriodic() {
-    	
-    }
-     
-    public void defenseTeleopPeriodic() {
-    	switch(DEFENSE_STATE){
-    	case: HOME
-    		
-    	break;
-    	}
-    }
 
+	final int MOVING = 1;
+	final int HOME = 2;
+	final int SALLY_PORT1 = 3;
+	final int SALLY_PORT2 = 4;
+	final int DRAW_BRIDGE1 = 5;
+	final int DRAW_BRIDGE2 = 6;
+	final int OTHER = 7;
+	final int MOVING_AWAY_FROM_HOME = 8;
+	final int MOVING_TOWARD_HOME = 9;
+	final int NOT_HOME = 10;
+
+	final int X = 11; // This will be the angle of the POT for the LongArm
+	final int Y = 12; // This will be the angle of the POT for the ShortArm
+	int DEFENSE_STATE = 13;
+	
+
+	public void defenseInit() {
+
+	}
+
+	public void defenseAutoPeriodic() {
+
+	}
+
+	public void defenseTeleopPeriodic() {
+		switch (DEFENSE_STATE) {
+		case HOME:
+			if (secondJoy.getRawButton(1)) {
+				if (!components.longArm.isFwdLimitSwitchClosed()) {
+					components.shortArm.set(1);
+					components.longArm.set(1);
+					DEFENSE_STATE = MOVING;
+				}
+			}
+
+			if (secondJoy.getRawButton(3)) {
+				if (!components.longArm.isFwdLimitSwitchClosed()) {
+					components.shortArm.set(1);
+					components.longArm.set(1);
+					DEFENSE_STATE = MOVING;
+				}
+			}
+			break;
+		case MOVING:
+			if (secondJoy.getRawButton(1)) {
+				if (components.longArm.getAnalogInPosition() == X && components.shortArm.getAnalogInPosition() == Y) {
+					components.shortArm.set(0);
+					components.longArm.set(0);
+					DEFENSE_STATE = SALLY_PORT1;
+				}
+			}
+			if (secondJoy.getRawButton(3)) {
+				if (components.longArm.getAnalogInPosition() == X && components.shortArm.getAnalogInPosition() == Y) {
+					components.shortArm.set(0);
+					components.longArm.set(0);
+					DEFENSE_STATE = DRAW_BRIDGE1;
+				}
+			}
+		}
+	}
 }
