b25edc93b6f76f545cf42c8f4f11dab95d24764f
Yang <yanglin501st@yahoo.com>
2016-01-29 22:16:58 -0500

    Added CommonArea

---
+++ Team3044-2016/src/com/team3044/robotmain/Reference/CommonArea.java
@@ -0,0 +1,27 @@
+package com.team3044.robotmain.Reference;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class CommonArea {
+	
+	//Drive
+	public double leftDriveSpeed;
+	public double rightDriveSpeed;
+	public boolean isManualDrive = true;
+	
+	//Shooter
+	public double shooterTopSpeed;
+	public double shooterBotSpeed;
+	public boolean aimFlag = false;
+	public boolean shooterMotorFlag = false;
+	public boolean shootFlag = false;
+	
+	//PickUp
+	public boolean portcullisFlag = false;
+	public boolean pickUpDrawBridgeFlag = false;
+	
+	//Arm
+	public boolean armDrawBridgeFlag = false;
+	
+	
+}
