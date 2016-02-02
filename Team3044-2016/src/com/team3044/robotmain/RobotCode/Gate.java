0bd4292f29f522e5cebfd33677c571819135ab0e
DAVIDAgustin <DAVIDAgustin@David-PC>
2016-02-02 14:56:34 -0500

    Added Gate Class

---
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Gate.java
@@ -0,0 +1,18 @@
+package com.team3044.robotmain.RobotCode;
+
+import com.team3044.robotmain.Reference.*;
+
+public class Gate {
+	Components components = new Components();
+
+	public void defenseInit() {
+
+	}
+
+	public void defenseAutoPeriodic() {
+
+	}
+
+	public void defenseTeleopPeriodic() {
+	}
+}
