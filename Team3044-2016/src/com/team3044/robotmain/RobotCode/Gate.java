bc4b5f603d65c5feae4376912cd8000ca25175c8
DAVIDAgustin <DAVIDAgustin@David-PC>
2016-02-02 18:57:57 -0500

    Added Gate Basic Code no Portculis

--- Team3044-2016/src/com/team3044/robotmain/RobotCode/Gate.java
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Gate.java
@@ -2,9 +2,33 @@ package com.team3044.robotmain.RobotCode;
 
 import com.team3044.robotmain.Reference.*;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class Gate {
 	Components components = new Components();
-
+	CommonArea commonarea = new CommonArea();
+	
+	//Inputs
+	boolean gateUp = commonarea.gateUp;
+	boolean gateDown = commonarea.gateDown;
+	boolean portcullisStart = commonarea.portcullisFlag;
+	//components.GateUpLimit.get()
+	//components.GateDownLimit.get()
+	//components.gateTalon.getEncPosition()
+	//components.BallInLimit.get()
+	
+	
+	//States
+	public enum state{
+		Stopped, movingUp, movingDown, autoPortculis
+	}
+	state gateState = state.Stopped;
+	
+	//Motor Speeds
+	double motorSpeedUp = 1;
+	double motorSpeedDown = 1;
+	
+	
 	public void gateInit() {
 
 	}
@@ -14,5 +38,35 @@ public class Gate {
 	}
 
 	public void gateTeleopPeriodic() {
+		switch (gateState){
+		
+		//STOPPED
+		case Stopped:
+			if (!components.GateUpLimit.get() && gateUp){
+				components.gateTalon.set(motorSpeedUp);
+				gateState = state.movingUp;
+			}else if (!components.GateDownLimit.get() && gateDown){
+				components.gateTalon.set(motorSpeedDown);
+				gateState = state.movingDown;
+			}else if (!components.BallInLimit.get() && portcullisStart){	//<---- look at this
+				gateState = state.autoPortculis;
+			}
+		
+		//MOVING UP
+		case movingUp:
+			if (components.GateUpLimit.get() || !gateUp){
+				components.gateTalon.set(0);
+				gateState = state.Stopped;
+			}
+		
+		//MOVING DOWN
+		case movingDown:
+			if (components.GateDownLimit.get() || !gateDown){
+				components.gateTalon.set(0);
+				gateState = state.Stopped;
+		}
+		
+		
 	}
+
 }
