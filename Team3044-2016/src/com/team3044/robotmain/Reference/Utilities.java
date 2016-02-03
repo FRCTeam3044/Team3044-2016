dde7217879d36609ca1b4b010b1b962063caa181
Yang <yanglin501st@yahoo.com>
2016-02-03 18:58:36 -0500

    Added Deadband

--- Team3044-2016/src/com/team3044/robotmain/Reference/Utilities.java
+++ Team3044-2016/src/com/team3044/robotmain/Reference/Utilities.java
@@ -3,13 +3,14 @@ package com.team3044.robotmain.Reference;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Utilities {
-//Place any functions in here
+
 	public static boolean tolerance(double lowerValue, double actualValue, double upperValue){
 		return (lowerValue < actualValue) && (actualValue < upperValue); 
 	}
+	
 	public static double deadband (double Value, double Tolerance){
 		if (-Tolerance < Value && Value < Tolerance){
-			return 0;
+			return 0; 
 		} else {
 			return Value;
 		}
