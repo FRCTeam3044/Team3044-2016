31356a1e75a85f0f3a18d2c582c4708ddd79347e
Yang <yanglin501st@yahoo.com>
2016-02-03 18:28:52 -0500

    Added Deadband

--- Team3044-2016/src/com/team3044/robotmain/Reference/Utilities.java
+++ Team3044-2016/src/com/team3044/robotmain/Reference/Utilities.java
@@ -7,4 +7,11 @@ public class Utilities {
 	public static boolean tolerance(double lowerValue, double actualValue, double upperValue){
 		return (lowerValue < actualValue) && (actualValue < upperValue); 
 	}
+	public static double deadband (double Value, double Tolerance){
+		if (-Tolerance < Value && Value < Tolerance){
+			return 0;
+		} else {
+			return Value;
+		}
+	}
 }
