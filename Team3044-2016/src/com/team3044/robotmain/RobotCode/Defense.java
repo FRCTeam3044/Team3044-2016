600523501ba1dd2f6b5c4fa8dcbdec5a122c0f13
Ryan Keihm <TheRyGuy125@gmail.com>
2016-02-03 20:47:23 -0500

    updated defense class

--- Team3044-2016/src/com/team3044/robotmain/RobotCode/Defense.java
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Defense.java
@@ -4,27 +4,22 @@ package com.team3044.robotmain.RobotCode;
 import com.team3044.robotmain.Reference.*;
 
 public class Defense {
-	Components components = new Components();
+
 	SecondaryController secondJoy = SecondaryController.getInstance();
 
-	final int MOVING = 1;
-	final int HOME = 2;
-	final int SALLY_PORT1 = 3;
-	final int SALLY_PORT2 = 4;
-	final int DRAW_BRIDGE1 = 5;
-	final int DRAW_BRIDGE2 = 6;
-	final int OTHER = 7;
-	final int MOVING_AWAY_FROM_HOME = 8;
-	final int MOVING_TOWARD_HOME = 9;
-	final int NOT_HOME = 10;
+	public enum state {
+		LA_MOVING_UP, LA_MOVING_UP_TARGET, LA_MOVING_DOWN, LA_MOVING_DOWN_TARGET, LA_CONFLICT, LA_STOPPED, 
+		UA_MOVING_UP, UA_MOVING_UP_TARGET, UA_MOVING_DOWN, UA_MOVING_DOWN_TARGET, UA_CONFLICT, UA_STOPPED, 
+		STOPPED, MOVING_MANUALLY, MOVING_TARGET
+	}
 
-	// Button 1 & 2 SALLY_PORT
-	// Button 3 & 4 DRAW_BRIDGE
-	// Butt 8 HOME
+	// Button 8 HOME
 
 	final int X = 11; // This will be the angle of the POT for the LongArm
 	final int Y = 12; // This will be the angle of the POT for the ShortArm
-	int DEFENSE_STATE = 13;
+	state LOWER_ARM = state.LA_STOPPED;
+	state MAIN = state.STOPPED;
+	state UPPER = state.UA_STOPPED;
 
 	public void defenseInit() {
 
@@ -35,145 +30,15 @@ public class Defense {
 	}
 
 	public void defenseTeleopPeriodic() {
-		switch (DEFENSE_STATE) {
-		case HOME:
-			if (secondJoy.getRawButton(1)) {
-				if (!components.longArm.isFwdLimitSwitchClosed()) {
-					components.shortArm.set(1);
-					components.longArm.set(1);
-					DEFENSE_STATE = MOVING_AWAY_FROM_HOME;
-				}
-			}
-
-			else if (secondJoy.getRawButton(3)) {
-				if (!components.longArm.isFwdLimitSwitchClosed()) {
-					components.shortArm.set(1);
-					components.longArm.set(1);
-					DEFENSE_STATE = MOVING_AWAY_FROM_HOME;
-				}
-			}
+		switch (LOWER_ARM)
+		{
+		case LA_STOPPED:
+			
+		default:
+			LOWER_ARM=state.LA_STOPPED;
 			break;
-		case MOVING_AWAY_FROM_HOME:
-			if (components.longArm.getAnalogInPosition() == X && components.shortArm.getAnalogInPosition() == Y) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING;
-			} else if (!secondJoy.getRawButton(1)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = NOT_HOME;
-			} else if (!secondJoy.getRawButton(3)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = NOT_HOME;
-				break;
-			}
-		case NOT_HOME:
-			if (secondJoy.getRawButton(1)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING_AWAY_FROM_HOME;
-			} else if (!secondJoy.getRawButton(3)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING_AWAY_FROM_HOME;
-			} else if (!secondJoy.getRawButton(8)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING_TOWARD_HOME;
-				break;
-			}
-		case MOVING_TOWARD_HOME:
-			if (components.longArm.getAnalogInPosition() == X && components.shortArm.getAnalogInPosition() == Y) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-			} else if (!secondJoy.getRawButton(1)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = NOT_HOME;
-			} else if (!secondJoy.getRawButton(3)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = NOT_HOME;
-				break;
-			}
-		case MOVING:
-			if (components.longArm.getAnalogInPosition() == X && components.shortArm.getAnalogInPosition() == Y) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = SALLY_PORT1;
-			} else if (components.longArm.getAnalogInPosition() == X
-					&& components.shortArm.getAnalogInPosition() == Y) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = DRAW_BRIDGE1;
-			} else if (components.longArm.getAnalogInPosition() == X
-					&& components.shortArm.getAnalogInPosition() == Y) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = SALLY_PORT2;
-			} else if (components.longArm.getAnalogInPosition() == X
-					&& components.shortArm.getAnalogInPosition() == Y) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = DRAW_BRIDGE2;
-			} else if (secondJoy.getRawButton(8)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = HOME;
-			} else if (secondJoy.getRawButton(8)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = HOME;
-			} else if (!secondJoy.getRawButton(1)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = OTHER;
-			} else if (!secondJoy.getRawButton(3)) {
-				components.shortArm.set(0);
-				components.longArm.set(0);
-				DEFENSE_STATE = OTHER;
-				break;
-			}
-		case SALLY_PORT1:
-			if (secondJoy.getRawButton(2)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING;
-				break;
-
-			}
-		case DRAW_BRIDGE1:
-			if (secondJoy.getRawButton(4)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING;
-				break;
-			}
-		case SALLY_PORT2:
-			if (secondJoy.getRawButton(1) || secondJoy.getRawButton(3) || secondJoy.getRawButton(4)
-					|| secondJoy.getRawButton(8)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING;
-				break;
-			}
-		case DRAW_BRIDGE2:
-			if (secondJoy.getRawButton(1) || secondJoy.getRawButton(2) || secondJoy.getRawButton(3)
-					|| secondJoy.getRawButton(8)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING;
-				break;
-			}
-		case OTHER:
-			if (secondJoy.getRawButton(1) || secondJoy.getRawButton(2) || secondJoy.getRawButton(3)
-					|| secondJoy.getRawButton(4) || secondJoy.getRawButton(8)) {
-				components.shortArm.set(1);
-				components.longArm.set(1);
-				DEFENSE_STATE = MOVING;
-				break;
-			}
+		case STOPPED:
 		}
+			
 	}
 }
