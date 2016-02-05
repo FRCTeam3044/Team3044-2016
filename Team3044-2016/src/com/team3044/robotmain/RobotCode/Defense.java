2d8eccbb34f993713456cedfe40efaaaaea6bf4d
Ryan Keihm <TheRyGuy125@gmail.com>
2016-02-04 20:43:17 -0500

    finished stopped state for defense

--- Team3044-2016/src/com/team3044/robotmain/RobotCode/Defense.java
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Defense.java
@@ -8,21 +8,44 @@ public class Defense {
 	SecondaryController secondJoy = SecondaryController.getInstance();
 
 	public enum state {
-		LA_MOVING_UP, LA_MOVING_UP_TARGET, LA_MOVING_DOWN, LA_MOVING_DOWN_TARGET, LA_CONFLICT, LA_STOPPED, UA_MOVING_UP, UA_MOVING_UP_TARGET, UA_MOVING_DOWN, UA_MOVING_DOWN_TARGET, UA_CONFLICT, UA_STOPPED, STOPPED, MOVING_MANUALLY, MOVING_TARGET
+		LA_MOVING_UP, LA_MOVING_UP_TARGET, LA_MOVING_DOWN, LA_MOVING_DOWN_TARGET, LA_CONFLICT, LA_STOPPED, 
+		UA_MOVING_UP, UA_MOVING_UP_TARGET, UA_MOVING_DOWN, UA_MOVING_DOWN_TARGET, UA_CONFLICT, UA_STOPPED, 
+		MAIN_STOPPED, MAIN_MOVING_MANUALLY, MAIN_MOVING_TARGET
 	}
 
 	// Button 8 HOME
 
-	final int X = 11; // This will be the angle of the POT for the LongArm
-	final int Y = 12; // This will be the angle of the POT for the ShortArm
+	public double lowerArmPOT = Components.longArm.getPosition();
+	public double upperArmPOT = Components.shortArm.getPosition();
+	
+	public boolean LA_UP;
+	public boolean LA_DOWN;
+	public boolean LA_UP_TARGET;
+	public boolean LA_DOWN_TARGET;
+	public boolean LA_TARGET_VALUE;
+	public boolean LA_STOP;
+	
+	public boolean UA_UP;
+	public boolean UA_DOWN;
+	public boolean UA_UP_TARGET;
+	public boolean UA_DOWN_TARGET;
+	public boolean UA_TARGET_VALUE;
+	public boolean UA_STOP;
+	
+	public double LA_MOVING_UP_SPEED;
+	public double LA_MOVING_DOWN_SPEED;
+	public double UA_MOVING_UP_SPEED;
+	public double UA_MOVING_DOWN_SPEED;
+	
+	
 	state LOWER_ARM = state.LA_STOPPED;
-	state MAIN = state.STOPPED;
+	state MAIN = state.MAIN_STOPPED;
 	state UPPER_ARM = state.UA_STOPPED;
+
 	public boolean LA_HOME = Components.shortArm.isFwdLimitSwitchClosed();
 	public boolean LA_TOO_FAR = Components.shortArm.isRevLimitSwitchClosed();
 	public boolean UA_HOME = Components.longArm.isFwdLimitSwitchClosed();
 	public boolean UA_TOO_FAR = Components.longArm.isRevLimitSwitchClosed();
-	
 
 	public void defenseInit() {
 
@@ -34,13 +57,30 @@ public class Defense {
 
 	public void defenseTeleopPeriodic() {
 		switch (LOWER_ARM) {
+		
 		default:
 			LOWER_ARM = state.LA_STOPPED;
 			break;
 
 		case LA_STOPPED:
-			Components.conflict.get();
+			if(LA_UP = true && !LA_TOO_FAR){
+				LOWER_ARM = state.LA_MOVING_UP;
+				Components.longArm.set(LA_MOVING_UP_SPEED);
+			}
+			if(LA_UP_TARGET = true && !LA_TOO_FAR){
+				LOWER_ARM = state.LA_MOVING_UP_TARGET;
+				Components.longArm.set(LA_MOVING_UP_SPEED);
+			}
+			if(LA_DOWN_TARGET = true && !LA_HOME){
+				LOWER_ARM = state.LA_MOVING_DOWN_TARGET;
+				Components.longArm.set(LA_MOVING_DOWN_SPEED);
+			}
+			if(LA_DOWN = true && !LA_HOME){
+				LOWER_ARM = state.LA_MOVING_DOWN;
+				Components.longArm.set(LA_MOVING_DOWN_SPEED);
+			}
 			break;
+			
 		case LA_CONFLICT:
 			break;
 		case LA_MOVING_UP:
@@ -53,7 +93,8 @@ public class Defense {
 			break;
 
 		}
-		switch (UPPER_ARM){
+		switch (UPPER_ARM) {
+
 		default:
 			UPPER_ARM = state.UA_STOPPED;
 			break;
@@ -69,6 +110,17 @@ public class Defense {
 		case UA_MOVING_UP_TARGET:
 			break;
 		case UA_MOVING_DOWN_TARGET:
+			break;
+		}
+		switch (MAIN) {
+
+		default:
+			MAIN = state.MAIN_STOPPED;
+			break;
+
+		case MAIN_MOVING_MANUALLY:
+			break;
+		case MAIN_MOVING_TARGET:
 			break;
 		}
 
