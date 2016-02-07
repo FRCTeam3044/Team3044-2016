8fccba4568d0ae9d38dab806eb347968e0815184
Yang <yanglin501st@yahoo.com>
2016-02-06 23:03:51 -0500

    Update Components.java
    Components

--- Team3044-2016/src/com/team3044/robotmain/Reference/Components.java
+++ Team3044-2016/src/com/team3044/robotmain/Reference/Components.java
@@ -7,59 +7,71 @@ import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Components {
-	
-	//Drive System 
-	public static CANTalon leftFrontDrive;
-	public static CANTalon leftBackDrive;
-	public static CANTalon rightFrontDrive;
-	public static CANTalon rightBackDrive;
-	
+
+	private static Components instance = null;
+
+	private Components() {
+
+	}
+ 
+	public static Components getInstance() {
+		if (instance == null) {
+			instance = new Components();
+		}
+		return instance;
+	}
+
+	// Drive System
+	public CANTalon leftFrontDrive;
+	public CANTalon leftBackDrive;
+	public CANTalon rightFrontDrive;
+	public CANTalon rightBackDrive;
+
 	public static double leftDriveEncoder;
 	public static double rightDriveEncoder;
-	
-	//Shooter
-	public static CANTalon topShooter;
-	public static CANTalon botShooter;
-	
-	//Defense
-	public static CANTalon lowerArm;
-	public static CANTalon upperArm;
-	
-	//Pick Up
-	public static CANTalon gateTalon;
-	public static CANTalon Pickup2;
-	public static CANTalon Pickup3;
 
-	
-	//Digital IOs
-	public static DigitalInput conflict = new DigitalInput(0);
-	public static DigitalInput topTacho = new DigitalInput(1);
-	public static DigitalInput botTacho = new DigitalInput(2);
-	
-	public static DigitalInput GateUpLimit = new DigitalInput(2);
-	public static DigitalInput GateDownLimit = new DigitalInput(3);
-	public static DigitalInput BallInLimit = new DigitalInput(4);
-	
-	public static Counter topTachoCounter = new Counter(topTacho);
-	public static Counter botTachoCounter = new Counter(botTacho);
-	
-		public void init(){
-			leftFrontDrive = new CANTalon(1);
-			leftBackDrive = new CANTalon(2);
-			rightFrontDrive = new CANTalon(3);
-			rightBackDrive = new CANTalon(4);
-			
-			leftFrontDrive.setFeedbackDevice(FeedbackDevice.AnalogEncoder);
-			rightFrontDrive.setFeedbackDevice(FeedbackDevice.AnalogEncoder);
-			
-			topShooter = new CANTalon(5);
-			botShooter = new CANTalon(6);
-			
-			lowerArm = new CANTalon(7);
-			upperArm = new CANTalon(8);
-			
-			gateTalon = new CANTalon(9);
-			Pickup2 = new CANTalon(10);
-			Pickup3 = new CANTalon(11);
+	// Shooter
+	public CANTalon topShooter;
+	public CANTalon botShooter;
+
+	// Defense
+	public CANTalon lowerArm;
+	public CANTalon upperArm;
+
+	// Pick Up
+	public CANTalon gateTalon;
+	public CANTalon Pickup2;
+	public CANTalon Pickup3;
+
+	// Digital IOs
+	public DigitalInput conflict = new DigitalInput(0);
+	public DigitalInput topTacho = new DigitalInput(1);
+	public DigitalInput botTacho = new DigitalInput(2);
+
+	public DigitalInput GateUpLimit = new DigitalInput(3);
+	public DigitalInput GateDownLimit = new DigitalInput(4);
+	public DigitalInput BallInLimit = new DigitalInput(5);
+
+	public Counter topTachoCounter = new Counter(topTacho);
+	public Counter botTachoCounter = new Counter(botTacho);
+
+	public void init() {
+		leftFrontDrive = new CANTalon(1);
+		leftBackDrive = new CANTalon(2);
+		rightFrontDrive = new CANTalon(3);
+		rightBackDrive = new CANTalon(4);
+
+		leftFrontDrive.setFeedbackDevice(FeedbackDevice.AnalogEncoder);
+		rightFrontDrive.setFeedbackDevice(FeedbackDevice.AnalogEncoder);
+
+		topShooter = new CANTalon(5);
+		botShooter = new CANTalon(6);
+
+		lowerArm = new CANTalon(7);
+		upperArm = new CANTalon(8);
+
+		gateTalon = new CANTalon(9);
+		Pickup2 = new CANTalon(10);
+		Pickup3 = new CANTalon(11);
 	}
 }
