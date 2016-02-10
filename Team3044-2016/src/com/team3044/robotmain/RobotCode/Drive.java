29102087e19139530bc9f5eed276ebe3f34ad8cf
alexlay <alexlay@172.30.31.227>
2016-02-09 19:18:00 -0500

    Added Autonomous Drive

--- Team3044-2016/src/com/team3044/robotmain/RobotCode/Drive.java
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Drive.java
@@ -8,15 +8,31 @@ public class Drive {
 
 	FirstController controller = FirstController.getInstance();
 
+	public enum state{
+		stopped, moveLeftMotor, moveRightMotor, moveBothMotors
+	}
+	state autoDriveState = state.stopped;
 	double leftAutoSpeed;
 	double rightAutoSpeed;
 	double leftDriveSpeed;
 	double rightDriveSpeed;
+	double rightDesiredEncoderValue;
+	double leftDesiredEncoderValue;
 
 	public CANTalon leftFrontDrive;
 	public CANTalon leftBackDrive;
 	public CANTalon rightFrontDrive;
 	public CANTalon rightBackDrive;
+	
+	public double encoderTolerance = 50;
+	
+	public boolean isAtDistance(double current, double desired){
+		if (desired + encoderTolerance < current || desired - encoderTolerance > current){
+			return true;
+		} else {
+			return false;
+		}
+	} 
 
 	public void driveInit() {
 
@@ -31,11 +47,17 @@ public class Drive {
 		leftFrontDrive.enableBrakeMode(true);
 		rightFrontDrive.enableBrakeMode(true);
 		leftBackDrive.enableBrakeMode(true);
-		rightBackDrive.enableBrakeMode(true);
+		rightBackDrive.enableBrakeMode(true);	
 	}
 
 	public void driveAutoPeriodic() {
-
+		boolean movexFeet = CommonArea.movexFeet;
+		double rightCurrentEncoderValue = rightFrontDrive.getAnalogInRaw();
+		double leftCurrentEncoderValue = leftFrontDrive.getAnalogInRaw();
+				
+		boolean leftOnTarget = isAtDistance(leftCurrentEncoderValue, leftDesiredEncoderValue);
+		boolean rightOnTarget = isAtDistance(rightCurrentEncoderValue, rightDesiredEncoderValue);
+		
 		leftAutoSpeed = CommonArea.leftAutoSpeed;
 		rightAutoSpeed = -CommonArea.rightAutoSpeed;
 
@@ -45,10 +67,74 @@ public class Drive {
 		if (Math.abs(rightAutoSpeed) < .1) {
 			rightAutoSpeed = 0;
 		}
+
 		leftFrontDrive.set(leftAutoSpeed);
 		leftBackDrive.set(leftAutoSpeed);
 		rightFrontDrive.set(rightAutoSpeed);
 		rightBackDrive.set(rightAutoSpeed);
+		
+		switch (autoDriveState){
+		case stopped:
+			if (!rightOnTarget && leftOnTarget && movexFeet){
+				rightFrontDrive.set(rightAutoSpeed);
+				rightBackDrive.set(rightAutoSpeed);
+				autoDriveState = state.moveRightMotor;	
+			
+			} else if (!leftOnTarget && rightOnTarget && movexFeet){
+				leftFrontDrive.set(leftAutoSpeed);
+				leftBackDrive.set(leftAutoSpeed);
+				autoDriveState = state.moveLeftMotor;
+			
+			} else if (!leftOnTarget && !rightOnTarget && movexFeet){
+				leftFrontDrive.set(leftAutoSpeed);
+				leftBackDrive.set(leftAutoSpeed);
+				rightFrontDrive.set(rightAutoSpeed);
+				rightBackDrive.set(rightAutoSpeed);
+				autoDriveState = state.moveBothMotors;
+			} else if (leftOnTarget && rightOnTarget && movexFeet){
+				CommonArea.atDistance = true;
+			}
+			break;
+		case moveLeftMotor:
+			if (rightOnTarget && leftOnTarget){
+				autoDriveState = state.stopped;
+			
+			} else if (!rightOnTarget && !leftOnTarget){
+				leftFrontDrive.set(leftAutoSpeed);
+				leftBackDrive.set(leftAutoSpeed);
+				rightFrontDrive.set(rightAutoSpeed);
+				rightBackDrive.set(rightAutoSpeed);
+				autoDriveState = state.moveBothMotors;
+			} 
+			break;
+		case moveRightMotor:
+			if (rightOnTarget && leftOnTarget){
+				autoDriveState = state.stopped;
+			
+			} else if (!rightOnTarget && !leftOnTarget){
+				leftFrontDrive.set(leftAutoSpeed);
+				leftBackDrive.set(leftAutoSpeed);
+				rightFrontDrive.set(rightAutoSpeed);
+				rightBackDrive.set(rightAutoSpeed);
+				autoDriveState = state.moveBothMotors;
+			}
+			break;
+		case moveBothMotors:
+			if (rightOnTarget && leftOnTarget){
+				autoDriveState = state.stopped;
+				
+			} else if (!rightOnTarget && leftOnTarget){
+				rightFrontDrive.set(rightAutoSpeed);
+				rightBackDrive.set(rightAutoSpeed);
+				autoDriveState = state.moveRightMotor;
+			
+			} else if (!leftOnTarget && rightOnTarget){
+				leftFrontDrive.set(leftAutoSpeed);
+				leftBackDrive.set(leftAutoSpeed);
+				autoDriveState = state.moveLeftMotor;
+			}
+			break;
+		}
 	}
 
 	public void driveTeleopPeriodic() {
