012198f1ef9126f2caed13936e7d440433fdd01b
Yang <yanglin501st@yahoo.com>
2016-02-11 21:39:23 -0500

    Update Robot.java
    Updated Robot

--- Team3044-2016/src/com/team3044/robotmain/RobotCode/Robot.java
+++ Team3044-2016/src/com/team3044/robotmain/RobotCode/Robot.java
@@ -4,32 +4,37 @@ import com.team3044.robotmain.Reference.CommonArea;
 import com.team3044.robotmain.Reference.Components;
 import com.team3044.robotmain.Reference.FirstController;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-	Defense defense = new Defense();
-	Drive drive = new Drive();
+	public 
+	//Defense defense = new Defense();
+	//Drive drive = new Drive();
 	Shooter shooter = new Shooter();
-	Gate gate = new Gate();
+	//Gate gate = new Gate();
+	//VisionCalc vision = new VisionCalc();
 
 	public void robotInit() {
 		Components.getInstance().init();
-		drive.driveInit();
-		defense.defenseInit();
+		//drive.driveInit();
+		//defense.defenseInit();
 		shooter.shooterInit();
-		gate.gateInit();
+		//gate.gateInit();
 	}
 
 	public void autonomousInit() {
-
+		
 	}
 
 	public void autonomousPeriodic() {
-		drive.driveAutoPeriodic();
-		defense.defenseAutoPeriodic();
-		shooter.shooterAutoPeriodic();
-		gate.gateAutoPeriodic();
+		//drive.driveAutoPeriodic();
+		//defense.defenseAutoPeriodic();
+		//shooter.shooterAutoPeriodic();
+		//gate.gateAutoPeriodic();
+		//vision.Vision();
 	}
 
 	public void teleopInit() {
@@ -37,13 +42,15 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopPeriodic() {
-		drive.driveTeleopPeriodic();
-		defense.defenseTeleopPeriodic();
+		//drive.driveTeleopPeriodic();
+		//defense.defenseTeleopPeriodic();
 		shooter.shooterTeleopPeriodic();
-		gate.gateTeleopPeriodic();
+		//gate.gateTeleopPeriodic();
+		//vision.Vision();
 	}
 
 	public void disabledInit() {
+
 	}
 
 	public void disabledPeriodic() {
@@ -54,33 +61,48 @@ public class Robot extends IterativeRobot {
 	final int DRIVE_FORWARD = 0;
 	final int DRIVE_BACK = 1;
 	final int DRIVE_NORMAL = 2;
-	final int ENCODER_DISTANCE_ONE_METER = 3000; // Need to change
+	final int ENCODER_DISTANCE_ONE_METER_Left = 3000; // Need to change
 	int count = 0;
 
 	public void testInit() {
 		count = 0;
 		TEST_DRIVE_STATE = DRIVE_NORMAL;
+		CommonArea.isManualDrive = true;
+		Components.getInstance().leftFrontDrive.setFeedbackDevice(FeedbackDevice.AnalogEncoder);
+		Components.getInstance().rightFrontDrive.setFeedbackDevice(FeedbackDevice.AnalogEncoder);
+		Components.getInstance().leftFrontDrive.setAnalogPosition(0);
+		Components.getInstance().rightFrontDrive.setAnalogPosition(0);
+		Components.getInstance().leftFrontDrive.setPosition(0);
+		Components.getInstance().rightFrontDrive.setPosition(0);
+		
+		//Components.getInstance().leftFrontDrive.getAnalogInPosition();
 	}
+	int subtract = 0;
 
 	public void testPeriodic() {
+		/*
+		//drive.testPeriodic();
+		//Components.getInstance().leftBackDrive.set(.3);
+		//Components.getInstance().leftFrontDrive.set(.3);
 
-		SmartDashboard
-				.putString("DB/String 0", String
-						.valueOf(Components.getInstance().leftFrontDrive
-								.getAnalogInPosition()));
-		SmartDashboard.putString("DB/String 1", String
-				.valueOf(Components.getInstance().rightFrontDrive.getAnalogInPosition()));
-
+		SmartDashboard.putString("DB/String 0", String.valueOf(Components
+				.getInstance().leftFrontDrive.getAnalogInPosition()));
+		SmartDashboard.putString("DB/String 1", String.valueOf(Components
+				.getInstance().rightFrontDrive.getAnalogInPosition()));
+		if(FirstController.getInstance().getRawButton(FirstController.BUTTON_B)){
+			testInit();
+		}
 		switch (TEST_DRIVE_STATE) {
 		case DRIVE_NORMAL:
 			if (FirstController.getInstance().getRawButton(
 					FirstController.BUTTON_START)) {
 				TEST_DRIVE_STATE = DRIVE_FORWARD;
-				count = 0;
+				//count = 0;
 				Components.getInstance().leftFrontDrive.setPosition(0);
 				Components.getInstance().rightFrontDrive.setPosition(0);
-				CommonArea.leftDriveSpeed = .5;
-				CommonArea.rightDriveSpeed = .5;
+				subtract = Components.getInstance().leftFrontDrive.getAnalogInPosition();
+				CommonArea.leftDriveSpeed = .3;
+				CommonArea.rightDriveSpeed = .3;
 				CommonArea.isManualDrive = false;
 			}
 			break;
@@ -89,23 +111,26 @@ public class Robot extends IterativeRobot {
 					FirstController.BUTTON_BACK)) {
 				CommonArea.leftDriveSpeed = 0;
 				CommonArea.rightDriveSpeed = 0;
+				
 				this.TEST_DRIVE_STATE = DRIVE_NORMAL;
 				CommonArea.isManualDrive = true;
 			}
 
-			if (Components.getInstance().leftFrontDrive.getAnalogInPosition() > ENCODER_DISTANCE_ONE_METER) {
+			if (Components.getInstance().leftFrontDrive.getAnalogInPosition()subtract < -ENCODER_DISTANCE_ONE_METER) {
 				CommonArea.leftDriveSpeed = 0;
+				
 			}
 
-			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() > ENCODER_DISTANCE_ONE_METER) {
+			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() /*subtract > ENCODER_DISTANCE_ONE_METER) {
 				CommonArea.rightDriveSpeed = 0;
 			}
 
-			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() > ENCODER_DISTANCE_ONE_METER
-					&& Components.getInstance().leftFrontDrive.getAnalogInPosition() > ENCODER_DISTANCE_ONE_METER) {
+			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() /*subtract> ENCODER_DISTANCE_ONE_METER
+					&& Components.getInstance().leftFrontDrive
+							.getAnalogInPosition() /*subtract < -ENCODER_DISTANCE_ONE_METER) {
 				TEST_DRIVE_STATE = DRIVE_BACK;
-				CommonArea.rightDriveSpeed = -.5;
-				CommonArea.leftDriveSpeed = -.5;
+				CommonArea.rightDriveSpeed = -.3;
+				CommonArea.leftDriveSpeed = -.3;
 			}
 
 			break;
@@ -118,21 +143,22 @@ public class Robot extends IterativeRobot {
 				CommonArea.isManualDrive = true;
 			}
 
-			if (Components.getInstance().leftFrontDrive.getAnalogInPosition() < 1) {
+			if (Components.getInstance().leftFrontDrive.getAnalogInPosition() /*subtract > -1) {
 				CommonArea.leftDriveSpeed = 0;
 			}
 
-			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() < 1) {
+			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() /*subtract < 1) {
 				CommonArea.rightDriveSpeed = 0;
 			}
 
-			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() < 1
-					&& Components.getInstance().leftFrontDrive.getAnalogInPosition() < 1) {
+			if (Components.getInstance().rightFrontDrive.getAnalogInPosition() /*subtract < 1
+					&& Components.getInstance().leftFrontDrive
+							.getAnalogInPosition() /*subtract> -1) {
 				if (count <= 5) {
 					TEST_DRIVE_STATE = DRIVE_FORWARD;
-					CommonArea.rightDriveSpeed = .5;
-					CommonArea.leftDriveSpeed = .5;
-					count ++;
+					CommonArea.rightDriveSpeed = .3;
+					CommonArea.leftDriveSpeed = .3;
+					count++;
 				} else {
 					TEST_DRIVE_STATE = DRIVE_NORMAL;
 					CommonArea.rightDriveSpeed = 0;
@@ -143,6 +169,8 @@ public class Robot extends IterativeRobot {
 
 			break;
 		}
+		
 		drive.driveTeleopPeriodic();
+		*/
 	}
 }
