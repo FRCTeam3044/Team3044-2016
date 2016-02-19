ccf8e8c268318628adefacb7bff7bbbc78fddf85
Yang <yanglin501st@yahoo.com>
2016-02-19 00:16:09 -0500

    Update FirstController.java
    Updated

--- Team3044-2016/src/com/team3044/robotmain/Reference/FirstController.java
+++ Team3044-2016/src/com/team3044/robotmain/Reference/FirstController.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class FirstController {
 	private static FirstController instance = null;
-	
+
 	private Joystick firstJoy;
-	
+
 	public static int BUTTON_X = 3;
 	public static int BUTTON_Y = 4;
 	public static int BUTTON_B = 2;
@@ -17,20 +17,18 @@ public class FirstController {
 	public static int BUTTON_LB = 5;
 	public static int BUTTON_BACK = 7;
 	public static int BUTTON_START = 8;
-	//ANALOG STICK BUTTONS STILL AVAILABLE
-	
+
 	private FirstController() {
 		firstJoy = new Joystick(0);
 	}
-	
+
 	public static FirstController getInstance() {
 		if (instance == null) {
 			instance = new FirstController();
 		}
-
 		return instance;
 	}
-	
+
 	public double getLeftX() {
 		return firstJoy.getRawAxis(0);
 	}
@@ -48,60 +46,53 @@ public class FirstController {
 	}
 
 	public boolean getTriggerRight() {
-		if(Math.abs(firstJoy.getRawAxis(3))>0){
+		return Math.abs(firstJoy.getRawAxis(3)) > 0.1;
+	}
+
+	public boolean getTriggerLeft() {
+		return Math.abs(firstJoy.getRawAxis(2)) > 0.1;
+	}
+
+	public boolean getDPadLeft() {
+		if (firstJoy.getPOV() == 270) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public boolean getTriggerLeft() {
-		if(Math.abs(firstJoy.getRawAxis(2))>0){
+	public boolean getDPadRight() {
+		if (firstJoy.getPOV() == 90) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	public boolean getDPadLeft(){
-		if(firstJoy.getPOV() == 270){
+
+	public boolean getDPadUp() {
+		if (firstJoy.getPOV() == 0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	public boolean getDPadRight(){
-		if(firstJoy.getPOV() == 90){
+
+	public boolean getDPadDown() {
+		if (firstJoy.getPOV() == 180) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	public boolean getDPadUp(){
-		if(firstJoy.getPOV() == 0){
+
+	public boolean getDPadOther() {
+		if (firstJoy.getPOV() == -1) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	public boolean getDPadDown(){
-		if(firstJoy.getPOV() == 180){
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public boolean getDPadOther(){
-		if(firstJoy.getPOV() == -1){
-			return true;
-		} else {
-			return false;
-		}
-	}
+
 	public boolean getRawButton(int num) {
 		if (!DriverStation.getInstance().isAutonomous()) {
 			return firstJoy.getRawButton(num);
@@ -110,5 +101,4 @@ public class FirstController {
 			return false;
 		}
 	}
-    
 }
