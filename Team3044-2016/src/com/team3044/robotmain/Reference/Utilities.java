5ecfbb0e7a6a4749875a35ca83657efae4f37560
Yang <yanglin501st@yahoo.com>
2016-02-19 00:19:09 -0500

    Updated

--- Team3044-2016/src/com/team3044/robotmain/Reference/Utilities.java
+++ Team3044-2016/src/com/team3044/robotmain/Reference/Utilities.java
@@ -1,7 +1,5 @@
 package com.team3044.robotmain.Reference;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-
 public class Utilities {
 
 	public static boolean tolerance(double lowerValue, double actualValue, double upperValue){
